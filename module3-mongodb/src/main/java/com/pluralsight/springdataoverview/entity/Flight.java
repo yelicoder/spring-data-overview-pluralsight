19db7fd958f8593b3dce625b094036f7a0e72b82
ye Li <yeli.88@gmail.com>
2021-04-14 09:45:37 -0400

    completed MongoDB migration

---
+++ module3-mongodb/src/main/java/com/pluralsight/springdataoverview/entity/Flight.java
@@ -0,0 +1,53 @@
+package com.pluralsight.springdataoverview.entity;
+
+import java.time.LocalDateTime;
+
+public class Flight {
+
+    private String id;
+    private String origin;
+    private String destination;
+    private LocalDateTime scheduledAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getScheduledAt() {
+        return scheduledAt;
+    }
+
+    public void setScheduledAt(LocalDateTime scheduledAt) {
+        this.scheduledAt = scheduledAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+            "id=" + id +
+            ", origin='" + origin + '\'' +
+            ", destination='" + destination + '\'' +
+            ", scheduledAt=" + scheduledAt +
+            '}';
+    }
+}
